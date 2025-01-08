1cc716866fe2f28ee8bedaeb84459e9e304890c6
Kei Ueno <ueno.keiichiro.r3@dc.tohoku.ac.jp>
2025-01-08 14:20:42 +0900

    add untracked files during rebase

--- space_robotics_bench/assets/robot/__init__.py
+++ space_robotics_bench/assets/robot/__init__.py
@@ -1,6 +1,7 @@
 import space_robotics_bench.core.assets as asset_utils
 import space_robotics_bench.core.envs as env_utils
 
+from .anymal import anymal_b_cfg, anymal_c_cfg, anymal_d_cfg  # noqa: F401
 from .canadarm3_large import canadarm3_large_cfg  # noqa: F401
 from .franka import franka_cfg
 from .ingenuity import ingenuity_cfg
@@ -14,6 +15,15 @@ def rover_from_env_cfg(
     **kwargs,
 ) -> asset_utils.MobileRobotCfg:
     return perseverance_cfg(prim_path=prim_path, **kwargs)
+
+
+def legged_robot_from_env_cfg(
+    env_cfg: env_utils.EnvironmentConfig,
+    *,
+    prim_path: str = "{ENV_REGEX_NS}/robot",
+    **kwargs,
+) -> asset_utils.LeggedRobotCfg:
+    return anymal_c_cfg(prim_path=prim_path, **kwargs)
 
 
 def manipulator_from_env_cfg(
